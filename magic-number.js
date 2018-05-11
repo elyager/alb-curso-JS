e97f4c19846a103abc0196c637638518516e8e34
Elyager <elyager@gmail.com>
2018-05-11 15:20:20 -0700

    Fix Problem with page not rendering before the game starts

--- magic-number.js
+++ magic-number.js
@@ -1,3 +1,4 @@
+
 var max = 10
 var min = 1
 var num = 0
@@ -6,27 +7,33 @@ var secret_num = Math.ceil(Math.random() * max)
 
 var mensaje = 'Trata de adivinar el número que estoy pensando del 1 al 10 '
 
-while (num !== secret_num) {
-  alert(secret_num)
-  num = prompt(mensaje)
+setTimeout(function() {
+  playing()
+}, 500);
 
-  num = Number(num) //Agregar validacion
+function playing() {
+  while (num !== secret_num) {
+    // alert(secret_num) //only show it if you want it to know the answer
+    num = prompt(mensaje)
 
-  if (num === 0) {
-    break;
+    num = Number(num) //Agregar validacion
+
+    if (num === 0) {
+      break;
+    }
+
+    if (num === secret_num) {
+      alert('Ganaste el número que estoy pensando si es ' + num)
+      break;
+    }
+
+    if (num > secret_num){
+      alert('Es un número menor < al que ingresate')
+    }
+    else {
+      alert('Es un número mayor > al que ingresate')
+    }
   }
 
-  if (num === secret_num) {
-    alert('Ganaste el número que estoy pensando si es ' + num)
-    break;
-  }
-
-  if (num > secret_num){
-    alert('Es un número menor al que ingresate')
-  }
-  else {
-    alert('Es un número mayor al que ingresate')
-  }
+  alert('Game Over')
 }
-
-alert('Game Over')
